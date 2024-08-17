40cfef6a7577797673ec710345ea98708e8d308b
PedroZelinski <pedrozelin@gmail.com>
2024-08-17 20:14:01 -0300

    banco de dados atualizado

--- Docs/datavisa.sql
+++ Docs/datavisa.sql
@@ -1,35 +1,49 @@
+CREATE SCHEMA `dataVisa`;
 use dataVisa;
+
+create table empresas(
+	id bigint primary key auto_increment,	
+    nome varchar(60),
+	empresa varchar(60) unique,
+	cnpj varchar(60)
+);
+
+INSERT INTO empresas (nome, empresa, cnpj) VALUES
+('DataVisa', 'DataVisa', '1'),
+('none', 'none', '0'),
+('Lojas Renner', 'Renner S/A', '00.000.000/0001-01'),
+('Magazine Luiza', 'Magazine Luiza S/A', '11.111.111/0001-11'),
+('Lojas Americanas', 'Americanas S/A', '22.222.222/0001-22'),
+('Casas Bahia', 'Casas Bahia Ltda', '33.333.333/0001-33'),
+('Riachuelo', 'Riachuelo S/A', '44.444.444/0001-44'),
+('Pizzaria', 'Pizzaria LTDA', '31.455.862/0001-24');
+
+
+select * from empresas;
 
 create table usuarios (
     email varchar(60) primary key,
     senha varchar(60),
     nome varchar(60),
-    departamento varchar(60),
-    edita_modelo int,
-    edita_conexao int,
-    nivel_acesso int
+    empresaId bigint,
+    permissaoTabela int not null default 0,
+    editaModelo int not null default 0,
+    editaConexao int not null default 0,
+    nivelAcesso int not null default 0,
+    FOREIGN KEY (empresaId) REFERENCES empresas(id)
 );
 
-insert into usuarios(nome, email, senha, departamento, 
-	edita_modelo, edita_conexao, nivel_acesso) values
-    ("Pedro", "pedro@fatec.com","1234","DataVisa",1,1,0),
-    ("Jorge", "analista@datavisa.com","qwerty","TI",0,1,1),
-    ("Lorena", "admin@datavisa.com","123456789","Administração",1,1,0),
-    ("William", "gestor@datavisa.com", "1q2w3e4r5t", "Gestão",0,1,1);
+select * from usuarios;
 
-create table empresas(
-	id bigint primary key auto_increment,	
-    nome varchar(60),
-	empresa varchar(60),
-	cnpj varchar(60)
-);
-
-INSERT INTO empresas (nome, empresa, cnpj) VALUES
-('Lojas Renner', 'Renner S/A', '00.000.000/0001-01'),
-('Magazine Luiza', 'Magazine Luiza S/A', '11.111.111/0001-11'),
-('Lojas Americanas', 'Americanas S/A', '22.222.222/0001-22'),
-('Casas Bahia', 'Casas Bahia Ltda', '33.333.333/0001-33'),
-('Riachuelo', 'Riachuelo S/A', '44.444.444/0001-44');
+insert into usuarios(nome, email, senha, empresaId, permissaoTabela, 
+	editaModelo, editaConexao, nivelAcesso) values
+    ("Pedro", "pedro@fatec.sp.gov.br","1234", 1, 0, 1, 1, 0),
+    ("Cido", "cido@visitante.com", "1234", 2, 0, 0, 1, 1),
+    ("Jorge", "analista@pizzaria.com", "qwerty", 8, 0, 0, 1, 1),
+    ("Lorena", "administracao@pizzaria.com","123456789",8,2, 1, 1, 0),
+    ("William", "gestor@pizzaria.com", "1q2w3e4r5t", 8, 1, 0, 1, 1),
+    ("Rebeca", "usuario@pizzaria.com", "1q2w3e4r5t", 8, 3, 0, 1, 1),
+    ("Ana", "ana@fatec.sp.gov.br","1234", 6, 2, 0, 0, 2);
 
 create table conexoes(
 	id bigint primary key auto_increment,
@@ -45,86 +59,48 @@ create table conexoes(
 );
 
 INSERT INTO conexoes (nomeConexao, tipoDb, nomeDb, usuarioDb, senhaDb, portDb, caminhoDb, empresaId) VALUES
-('Renner_Conexao1', 'MySQL', 'db_renner_1', 'user_renner', 'senha123', 3306, '/caminho/para/db1', 1),
-('Magalu_Conexao1', 'MySQL', 'db_magalu_1', 'user_magalu', 'senha123', 3306, '/caminho/para/db1', 2),
-('Americanas_Conexao1', 'MySQL', 'db_americanas_1', 'user_americanas', 'senha123', 3306, '/caminho/para/db1', 3),
-('CasasBahia_Conexao1', 'MySQL', 'db_casasbahia_1', 'user_casasbahia', 'senha123', 3306, '/caminho/para/db1', 4),
-('Riachuelo_Conexao1', 'MySQL', 'db_riachuelo_1', 'user_riachuelo', 'senha123', 3306, '/caminho/para/db1', 5);
+('Renner_Conexao1', 'MySQL', 'db_renner_1', 'user_renner', 'senha123', 3306, '/db_renner_1', 3),
+('Magalu_Conexao1', 'MySQL', 'db_magalu_1', 'user_magalu', 'senha123', 3306, '/caminho/para/db1', 4),
+('Americanas_Conexao1', 'MySQL', 'db_americanas_1', 'user_americanas', 'senha123', 3306, '/caminho/para/db1', 5),
+('CasasBahia_Conexao1', 'MySQL', 'db_casasbahia_1', 'user_casasbahia', 'senha123', 3306, '/caminho/para/db1', 6),
+('Riachuelo_Conexao1', 'MySQL', 'db_riachuelo_1', 'user_riachuelo', 'senha123', 3306, '/caminho/para/db1', 7),
+('Pizzaria_Conexao1', 'MySQL', 'Pizzaria_bd', 'root', '1234', 3306, '/pizzaria_db', 8);
 
-CREATE TABLE Renner_Conexao1 (
+select * from conexoes;
+
+CREATE TABLE tabelas_db_renner_1 (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60),
-    campos VARCHAR(100),
     conexaoId BIGINT,     
     permissaoAcesso INT,
     FOREIGN KEY (conexaoId) REFERENCES conexoes(id) 
 );
 
-CREATE TABLE Magalu_Conexao1 (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(60),
-    campos VARCHAR(100),
-    conexaoId BIGINT,     
-    permissaoAcesso INT,
-    FOREIGN KEY (conexaoId) REFERENCES conexoes(id) 
-);
+INSERT INTO tabelas_db_renner_1 (nome, conexaoId, permissaoAcesso) VALUES
+('clientes', 1, 3),
+('vendedores', 1, 2),
+('vendas',  1, 3),
+('entregas', 1, 3);
 
-CREATE TABLE Americanas_Conexao1 (
+CREATE TABLE tabelas_Pizzaria_bd (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60),
-    campos VARCHAR(100),
-    conexaoId BIGINT,     
-    permissaoAcesso INT,
-    FOREIGN KEY (conexaoId) REFERENCES conexoes(id) 
-);
-
-CREATE TABLE CasasBahia_Conexao1 (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(60),
-    campos VARCHAR(100),
     conexaoId BIGINT,     
     permissaoAcesso INT, 
     FOREIGN KEY (conexaoId) REFERENCES conexoes(id) 
 );
 
-CREATE TABLE Riachuelo_Conexao1 (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(60),
-    campos VARCHAR(100),
-    conexaoId BIGINT,     
-    permissaoAcesso INT, 
-    FOREIGN KEY (conexaoId) REFERENCES conexoes(id) 
-);
+INSERT INTO tabelas_Pizzaria_bd (nome, conexaoId, permissaoAcesso) VALUES
+('Colaboradores', 6, 3),
+('Clientes', 6, 3),
+('Produtos', 6, 3),
+('Fornecedores', 6, 3),
+('NotasFiscais', 6, 2),
+('Vendas', 6, 3),
+('Financeiro', 6, 1);
 
-INSERT INTO Renner_Conexao1 (nome, campos, conexaoId, permissaoAcesso, permissaoAlteracao) VALUES
-('contratos', 'id<BIGINT>, nome<VARCHAR>, valor<DECIMAL>, vencimento<DATE>', 1, 2),
-('vendedor', 'id<BIGINT>, nome<VARCHAR>, salario<DOUBLE>, percComissao<DOUBLE>', 1, 3),
-('clientes', 'id<BIGINT>, nome<VARCHAR>, documento<VARCHAR>, telefone<VARCHAR>', 1, 3),
-('vendas', 'id<BIGINT>, nroCupom<int>, dataVenda<DATETIME>, total<DOUBLE>, clienteId<BIGINT>,vendedorId<BIGINT>', 1, 3),
-('entregas', 'id<BIGINT>, vendaId<BIGINT>, endereco<VARCHAR>, status<VARCHAR>, dataEntrega<DATE>', 1, 3);
+select * from tabelas_Pizzaria_bd;
 
-
-INSERT INTO Magalu_Conexao1 (nome, campos, conexaoId, permissaoAcesso, permissaoAlteracao) VALUES
-('Produtos_Magalu', 'id<BIGINT>, nome<VARCHAR>, preco<DECIMAL>, estoque<INT>', 2, 3),
-('Clientes_Magalu', 'id<BIGINT>, nome<VARCHAR>, email<VARCHAR>, telefone<VARCHAR>', 2, 3),
-('Vendas_Magalu', 'id<BIGINT>, data<DATETIME>, clienteId<BIGINT>, total<DECIMAL>', 2, 3);
-
-INSERT INTO Americanas_Conexao1 (nome, campos, conexaoId, permissaoAcesso, permissaoAlteracao) VALUES
-('Produtos_Americanas', 'id<BIGINT>, nome<VARCHAR>, preco<DECIMAL>, estoque<INT>', 3, 3),
-('Clientes_Americanas', 'id<BIGINT>, nome<VARCHAR>, email<VARCHAR>, telefone<VARCHAR>', 3, 3),
-('Vendas_Americanas', 'id<BIGINT>, data<DATETIME>, clienteId<BIGINT>, total<DECIMAL>', 3, 3);
-
-
-INSERT INTO CasasBahia_Conexao1 (nome, campos, conexaoId, permissaoAcesso, permissaoAlteracao) VALUES
-('Produtos_CasasBahia', 'id<BIGINT>, nome<VARCHAR>, preco<DECIMAL>, estoque<INT>', 4, 3),
-('Clientes_CasasBahia', 'id<BIGINT>, nome<VARCHAR>, email<VARCHAR>, telefone<VARCHAR>', 4, 3),
-('Vendas_CasasBahia', 'id<BIGINT>, data<DATETIME>, clienteId<BIGINT>, total<DECIMAL>', 4, 3);
-
-
-INSERT INTO Riachuelo_Conexao1 (nome, campos, conexaoId, permissaoAcesso, permissaoAlteracao) VALUES
-('Produtos_Riachuelo', 'id<BIGINT>, nome<VARCHAR>, preco<DECIMAL>, estoque<INT>', 5, 3),
-('Clientes_Riachuelo', 'id<BIGINT>, nome<VARCHAR>, email<VARCHAR>, telefone<VARCHAR>', 5, 3),
-('Vendas_Riachuelo', 'id<BIGINT>, data<DATETIME>, clienteId<BIGINT>, total<DECIMAL>', 5, 3);
-
+select permissaoAcesso from  tabelas_Pizzaria_bd where nome = 'NotasFiscais';
 
 
