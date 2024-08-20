0a9cd20b708b0190fdf6bb58bd76fbf173ada71a
PedroZelinski <pedrozelin@gmail.com>
2024-08-20 09:07:51 -0300

    queries para criação das tabelas de permissão adicionadas

--- Docs/datavisa.sql
+++ Docs/datavisa.sql
@@ -10,13 +10,33 @@ create table empresas(
 
 INSERT INTO empresas (nome, empresa, cnpj) VALUES
 ('DataVisa', 'DataVisa', '1'),
-('none', 'none', '0'),
-('Lojas Renner', 'Renner S/A', '00.000.000/0001-01'),
+('Sem_empresa', 'sem_empresa', '0'),
+('Renner', 'Renner S/A', '00.000.000/0001-01'),
 ('Magazine Luiza', 'Magazine Luiza S/A', '11.111.111/0001-11'),
 ('Lojas Americanas', 'Americanas S/A', '22.222.222/0001-22'),
 ('Casas Bahia', 'Casas Bahia Ltda', '33.333.333/0001-33'),
 ('Riachuelo', 'Riachuelo S/A', '44.444.444/0001-44'),
 ('Pizzaria', 'Pizzaria LTDA', '31.455.862/0001-24');
+
+create table DataVisa_permissoes(
+	permissao_tabela int primary key,	
+    nome varchar(60),
+    empresaId bigint,
+	FOREIGN KEY (empresaId) REFERENCES empresas(id)
+);
+
+INSERT INTO DataVisa_permissoes (permissao_tabela, nome, empresaId) VALUES
+(0, 'DataVisa', 1);
+
+create table Sem_empresa_permissoes(
+	permissao_tabela int primary key,	
+    nome varchar(60),
+    empresaId bigint,
+	FOREIGN KEY (empresaId) REFERENCES empresas(id)
+);
+
+INSERT INTO Sem_empresa_permissoes (permissao_tabela, nome, empresaId) VALUES
+(100, 'Sem_empresa', 2);
 
 create table usuarios (
     email varchar(60) primary key,
@@ -33,12 +53,12 @@ create table usuarios (
 insert into usuarios(nome, email, senha, empresaId, permissaoTabela, 
 	editaModelo, editaConexao, nivelAcesso) values
     ("Pedro", "pedro@fatec.sp.gov.br","1234", 1, 0, 1, 1, 0),
-    ("Rebeca", "rebeca@pizzaria.com", "1234", 8, 3, 0, 1, 1),
+    ("Rebeca", "rebeca@pizzaria.com", "1234", 8, 3, 1, 1, 1),
+    ("Luiz", "luiz@renner.com", "1234", 3, 3, 0, 0, 1),
     ("Cido", "cido@visitante.com", "1234", 2, 0, 0, 0, 2),
     ("Jorge", "analista@pizzaria.com", "qwerty", 8, 0, 0, 1, 1),
     ("Lorena", "administracao@pizzaria.com","123456789",8,2, 1, 1, 0),
     ("William", "gestor@pizzaria.com", "1q2w3e4r5t", 8, 1, 0, 1, 1),
-    ("Luiz", "luiz@renner.com", "1234", 3, 2, 1, 1, 1),
     ("Ana", "ana@fatec.sp.gov.br","1234", 6, 2, 0, 0, 2);
 
 create table conexoes(
@@ -63,6 +83,19 @@ INSERT INTO conexoes (nomeConexao, tipoDb, nomeDb, usuarioDb, senhaDb, hostName 
 ('Riachuelo_Conexao1', 'MySQL', 'db_riachuelo_1', 'root', '1234', 'localhost', 3306, '/caminho/para/db1', 7),
 ('Pizzaria_Conexao1', 'MySQL', 'Pizzaria_bd', 'root', '1234', 'localhost', 3306, '/pizzaria_db', 8);
 
+create table Renner_permissoes(
+	permissao_tabela int primary key,	
+    nome varchar(60),
+    empresaId bigint,
+	FOREIGN KEY (empresaId) REFERENCES empresas(id)
+);
+
+INSERT INTO Renner_permissoes (permissao_tabela, nome, empresaId) VALUES
+(0, 'Administrador', 3),
+(1, 'Gestor', 3),
+(2, 'Gerente', 3),
+(3, 'Atendente', 3);
+
 CREATE TABLE tabelas_db_renner_1 (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60),
@@ -77,6 +110,18 @@ INSERT INTO tabelas_db_renner_1 (nome, conexaoId, permissaoAcesso) VALUES
 ('vendas',  1, 3),
 ('entregas', 1, 3);
 
+create table Pizzaria_permissoes(
+	permissao_tabela int primary key,	
+    nome varchar(60),
+    empresaId bigint,
+	FOREIGN KEY (empresaId) REFERENCES empresas(id)
+);
+
+INSERT INTO Pizzaria_permissoes (permissao_tabela, nome, empresaId) VALUES
+(0, 'Administrador', 8),
+(1, 'Gerente', 8),
+(2, 'Atendente', 8);
+
 CREATE TABLE tabelas_Pizzaria_bd (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60),
@@ -86,10 +131,10 @@ CREATE TABLE tabelas_Pizzaria_bd (
 );
 
 INSERT INTO tabelas_Pizzaria_bd (nome, conexaoId, permissaoAcesso) VALUES
-('Colaboradores', 6, 3),
-('Clientes', 6, 3),
-('Produtos', 6, 3),
-('Fornecedores', 6, 3),
-('NotasFiscais', 6, 2),
-('Vendas', 6, 3),
-('Financeiro', 6, 1);
+('Colaboradores', 6, 2),
+('Clientes', 6, 2),
+('Produtos', 6, 2),
+('Fornecedores', 6, 2),
+('NotasFiscais', 6, 1),
+('Vendas', 6, 2),
+('Financeiro', 6, 0);
