3f4595579260627e5cf988d0eb554c225010a2ed
PedroZelinski <pedrozelin@gmail.com>
2024-09-28 12:41:39 -0300

    Estrutura templates

--- DataVisa-Backend/src/main/java/com/DataVisa/Models/TemplateModel.java
+++ DataVisa-Backend/src/main/java/com/DataVisa/Models/TemplateModel.java
@@ -2,29 +2,17 @@ package com.DataVisa.Models;
 
 import java.io.Serializable;
 
+import com.DataVisa.Models.Inferfaces.ArtifactModel;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "templates")
-@Data
-public class TemplateModel implements Serializable {
+public class TemplateModel extends ArtifactModel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	//nome teste
-	//relatorio 1
-	@Id
-	@GeneratedValue
-	private Long id;
 	
-	private String nome;
 	
-	private String corpo;
-	
-	private Long empresaId;
-	
-} 
+}  
