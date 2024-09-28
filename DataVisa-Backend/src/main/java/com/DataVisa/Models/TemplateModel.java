f4463da7c0aa0a40b263c303b2ccca245607c6ec
PedroZelinski <pedrozelin@gmail.com>
2024-09-28 16:41:01 -0300

    Fix template

--- DataVisa-Backend/src/main/java/com/DataVisa/Models/TemplateModel.java
+++ DataVisa-Backend/src/main/java/com/DataVisa/Models/TemplateModel.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import com.DataVisa.Models.Inferfaces.ArtifactModel;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,6 +15,10 @@ import jakarta.persistence.Table;
 public class TemplateModel extends ArtifactModel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	
 	
