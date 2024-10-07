03d501ec4cc3d54a81dfdb932bee99762b68daca
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-10-07 17:37:54 -0300

    bat pra instalar bibliotecas do front
    - criado um arquivo .bat para instalar as bibliotecas do front
    - removido o .txt com as bibliotecas

---
+++ Docs/bibliotecas_front.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd ../DataVisa-Frontend
+echo .
+echo Instalando bibliotecas do frontend
+echo .
+call npm i primeflex
+call npm i primereact
+call npm i axios
+call npm i react-router-dom
+call npm i @flaticon/flaticon-uicons
+call npm i react-plotly.js plotly.js
+echo .
+echo .
+echo Bibliotecas instaladas
+pause
