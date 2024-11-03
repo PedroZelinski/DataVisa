9bbc806b8042a456c7d0ae55dee8da4b65da13c5
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-11-03 19:47:07 -0300

    bibliotecas front atualizadas
    - bat atualizado com as novas bibliotecas usadas pra gerar pdfs

--- Docs/bibliotecas_front.bat
+++ Docs/bibliotecas_front.bat
@@ -10,6 +10,8 @@ call npm i axios
 call npm i react-router-dom
 call npm i @flaticon/flaticon-uicons
 call npm i react-plotly.js plotly.js
+call npm i npm install html2canvas
+call npm i npm install jspdf
 echo .
 echo .
 echo Bibliotecas instaladas
