33fc4e93dccc188b791d56e94476a7bd87db4345
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-11-03 19:46:01 -0300

    geracao de pdf
    - implementado download de pdf do grafico com html2canvas e jspdf

--- DataVisa-Frontend/src/pages/subpage-menu/Gerar.js
+++ DataVisa-Frontend/src/pages/subpage-menu/Gerar.js
@@ -2,9 +2,25 @@ import React from 'react'
 import logo from '../../assets/logoOriginal.png'
 import Pizza from '../../components/Templates/Pizza.js'
 import { useNavigate } from 'react-router-dom'
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 const Gerar = () => {
   const navigate = useNavigate()
+
+  const printDoc = () => {
+    const input = document.getElementById('resultado')
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF(
+        {orientation: "landscape",
+          format: [300, 150]
+        }
+      )
+      pdf.addImage(imgData, 'JPEG', 0, 0)
+      pdf.save("DataVisa.pdf")
+    })
+  }
 
   return (
     <div className='col-12'>
@@ -21,25 +37,28 @@ const Gerar = () => {
         </div>
 
         <div className="card-area grid ml-3 mb-3 mr-3 w-full">
-          <div className="col-2 mt-4 text-right">
-            <img src={logo} alt="" style={{ height: "90px" }} />
-          </div>
+          
+          <div className='col-9 grid' id='resultado'>
+            <div className="col-2 mt-4 ml-4">
+              <img src={logo} alt="" style={{ height: "90px" }} />
+            </div>
 
-          <div className="col-7">
-            <Pizza
-              valores={["25", "30", "67", "6"]}
-              labels={["Text 1", "Text 2", "Text 3", "Text 4"]}
-              layout={
-                {
-                  width: 535,
-                  height: 350,
-                  title: "Grafico de Teste",
-                  margin: {
-                    r: 30, l: 110, t: 40, b: 20
+            <div className="col-9">
+              <Pizza
+                valores={["25", "30", "67", "6"]}
+                labels={["Text 1", "Text 2", "Text 3", "Text 4"]}
+                layout={
+                  {
+                    width: 535,
+                    height: 350,
+                    title: "Grafico de Teste",
+                    margin: {
+                      r: 30, l: 110, t: 40, b: 20
+                    }
                   }
                 }
-              }
-            />
+              />
+            </div>
           </div>
 
           <div className="col-3 text-center">
@@ -49,11 +68,12 @@ const Gerar = () => {
               </button>
             </div>
             <div className="col-12">
-              <button className='gerar-btn'>
+              <button className='gerar-btn' onClick={() => printDoc()}>
                 Baixar <i className='fi-rr-download ml-1' />
               </button>
             </div>
           </div>
+
         </div>
       </div>
     </div>
